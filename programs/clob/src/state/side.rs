3a2877a8c860613220550921fafbb94dc8c28db0
metaproph3t <metaproph3t@protonmail.com>
2023-06-06 00:00:00 +0000

    Add `Side` enum
    Because `Side` will need to be stored in a zero-copy account, and
    because enums don't work well with zero-copy, I've added a `StoredSide`
    struct that stores `Side` in `u8` representation.

---
+++ programs/clob/src/state/side.rs
@@ -0,0 +1,29 @@
+use super::*;
+
+#[repr(u8)]
+pub enum Side {
+    Buy = 0,
+    Sell = 1,
+}
+
+// An enum in a form that is zero-copyable
+#[zero_copy]
+pub struct StoredSide {
+    pub inner: u8,
+}
+
+impl From<Side> for StoredSide {
+    fn from(side: Side) -> Self {
+        Self { inner: side as u8 }
+    }
+}
+
+impl From<StoredSide> for Side {
+    fn from(stored_side: StoredSide) -> Self {
+        match stored_side.inner {
+            0 => Side::Buy,
+            1 => Side::Sell,
+            _ => unreachable!(),
+        }
+    }
+}
